25c020cf486e0e0d0f0f7d46a880492994b1d461
2sammy <samuelmanoah61@gmail.com>
2025-03-05 00:12:47 +0300

    introduction

--- test.sql
+++ test.sql
@@ -5,3 +5,46 @@ using sqliteviz database about job posting
 
 SELECT *
 FROM job_postings_fact
+
+--explation from the above code
+SELECT - Identifies the columns (data) from database
+FROM Identifies the the table we are connecting to
+* - Special command to select all the columns
+
+-- selecting specific columns
+SELECT
+ job_id,
+ job_title,
+ job_location
+FROM job_postings_fact
+
+--Limit keyword
+- it's used only to querry a certain amount of rows
+
+SELECT
+ job_id,
+ job_title,
+ job_location
+FROM job_postings_fact
+Limit 10
+
+-- Distinct keyword\
+-Means getting unique rows
+    .takes alot of processing power
+
+SELECT DISTINCT
+ job_id,
+ job_title,
+ job_location
+FROM job_postings_fact
+Limit 10
+
+--Where keyword
+    -Sets a condition for the query
+    SELECT 
+ job_id,
+ job_title,
+ job_location
+FROM job_postings_fact
+WHERE job_title = 'Data Analyst'
+Limit 10
