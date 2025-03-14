eda2f601c9235376948d0961095ecb713b4977c9
2sammy <samuelmanoah61@gmail.com>
2025-03-15 00:15:01 +0300

    joins

--- Joins.sql
+++ Joins.sql
@@ -34,8 +34,35 @@ Full order of execution
 
     where
 -Filters rows based on conditions
+*/
 
+
+/*
+INSERT INTO
+insert into pupilstable (column1, column2)
+values (value1, value2)
+# INSERT INTO statement is used to insert new records in a table
+    INSERT INTO table_name (column1, column2, column3, ...)
+    VALUES (value1, value2, value3, ...);
+
+    or adding values for all the columns of the table
+    INSERT INTO table_name
+VALUES (value1, value2, value3, ...);
 
 
 */
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+-- inserting data in specific columns
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
+
+--inserting multiple rows
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES
+('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
+('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
+('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK')
 
